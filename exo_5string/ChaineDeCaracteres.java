854e53fc5e88151cad8b1b9917d616285c3bd0fb
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-02-23 16:36:42 +0100

    stringexo

---
+++ exo_5string/ChaineDeCaracteres.java
@@ -0,0 +1,14 @@
+package exo_5string;
+
+public class ChaineDeCaracteres {
+    public static void main(String[] args) {
+
+        // Définition et déclaration d'une chaîne valant "Hey ?"
+        String coucou = "Hey ?";
+        // Définition et déclaration d'une chaîne valant "What's wrong with you ?"
+        String grumpf = "What's wrong with you";
+        // Affichage de : "Hey ? What's wrong with you ?" dans la console
+        System.out.println(coucou + ' ' + grumpf);
+    }
+
+}
