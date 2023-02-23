820379a07449661877da10d736e2af371b929415
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-02-23 16:45:22 +0100

    bonus strings

--- exo_5string/ChaineDeCaracteres.java
+++ exo_5string/ChaineDeCaracteres.java
@@ -6,12 +6,12 @@ public class ChaineDeCaracteres {
         // Définition et déclaration d'une chaîne valant "Hey ?"
         String coucou = "Hey ?";
         // Définition et déclaration d'une chaîne valant "What's wrong with you ?"
-        String grumpf = "What's wrong with you";
+        String grumpf = "What's wrong with you ?";
         // Affichage de : "Hey ? What's wrong with you ?" dans la console
         System.out.println(coucou + ' ' + grumpf);
         System.out.println(grumpf.substring(4, 5));
         System.out.println(grumpf.length());
-        System.out.println(grumpf.substring(11, 21).toUpperCase());
+        System.out.println(grumpf.substring(13, 23).toUpperCase());
    
     }
 
