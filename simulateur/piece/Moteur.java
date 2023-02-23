cba8ea35d358b2d2c30a5249f622cdf61f18f40e
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-02-23 15:58:38 +0100

    nettoyage constructeur Bateau com

--- simulateur/piece/Moteur.java
+++ simulateur/piece/Moteur.java
@@ -1,7 +1,7 @@
 package simulateur.piece;
 
 public class Moteur {
-    private final int puissance ;
+    private final int puissance;
     private final int consommation;
 
     public int getPuissance() {
@@ -10,6 +10,11 @@ public class Moteur {
     public int getConsommation() {
         return consommation;
     }
+    // intéressant de faire un constructeur ici 
+    // car permet de déterminer des valeurs particulières pour chaque instance
+    // plutôt que les mêmes pour toutes les instances de la classe
+    // (cela en affectant dès le début des valeurs aux attributs de la classe
+    // qui seront fixes puisque final)
     public Moteur(int puissance, int consommation){
         this.puissance= puissance;
         this.consommation = consommation;
