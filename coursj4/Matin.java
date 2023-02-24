c2fe43f38b3a3868549107197bff9d1aa8cc859a
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-02-24 12:19:40 +0100

    exo conditions

--- coursj4/Matin.java
+++ coursj4/Matin.java
@@ -1,26 +1,86 @@
 package coursj4;
 
+import java.util.Scanner;
 public class Matin {
-    //type primitif et wrapper
-    public static void main(String[]args){
-        for(int i = 5; i<10; i++){
+    // type primitif et wrapper
+    int a = scannerClavier.nextInt();
+        scannerClavier.nextLine();
+    public static void main(String[] args) {
+        for (int i = 5; i < 10; i++) {
             System.out.println(i);
         }
 
         // boucle while
         int j = 15;
-        while(j < 20){
+        while (j < 20) {
             System.out.println(j);
             j++;
         }
 
-        //boucle do while
+        // boucle do while
         int k = 0;
         do {
             System.out.println("Mon compteur est de : " + k);
             System.out.println("coucou");
             k++;
-        } while(k<5);
+        } while (k < 5);
+
+        // condition
+
+        short forceElephant = 10;
+        short forceRhino = 12;
+        if (forceElephant > forceRhino) {
+            System.out.println("Bravo l'éléphant !");
+        } else {
+            System.out.println("Bravo le rhino !");
+        }
+
+        switch (forceElephant) {
+            case 1:
+                System.out.println("La force vaut 1");
+                break;
+            case 5:
+                System.out.println("La force vaut 5");
+                break;
+            case 10:
+                System.out.println("La force vaut 10");
+                break;
+            default:
+                System.out.println("La force a une valeur inconnue");
+                break;
+        }
+        int resultat = 0;
+        switch (forceElephant) {
+            case 1, 5, 7 -> {
+                System.out.println("L'éléphant est plutôt fort");
+                resultat = 5;
+            }
+            case 10 -> {
+                System.out.println("L'éléphant est fort");
+                resultat = 10;
+            }
+            default -> {
+                System.out.println("Je ne connais pass la force de l'éléphant");
+            }
+        }
+
+        int forceEnNewton = switch (forceElephant) {
+            case 10 -> {
+                yield 200;
+            }
+            default -> {
+                yield 100;
+            }
+        };
+        System.out.println(forceEnNewton);
+        // cette strucutre doit se terminer par un ;
+        // car il y a une affectation = une instruction à l'intéreieur
+
+        // Ternaire
+        String force = forceElephant > forceRhino ? "ok" : "not ok";
+        System.out.println(force);
+        System.out.println(forceElephant > forceRhino ? forceElephant == forceRhino ? "ok" : "not ok" : "dacc");
+        
+        // permet d'examiner 3 conditions mais dégueu à ne pas faire car peu lisible
     }
-    
 }
---
+++ exo_8conditions/ExoConditions.java
@@ -0,0 +1,57 @@
+package exo_8conditions;
+
+import java.util.Scanner;
+
+public class ExoConditions {
+
+    public static void main(String[] args) {
+        // Récupération de deux variables a et b
+        Scanner scannerClavier = new Scanner(System.in);
+        System.out.println("Veuillez entrer un entier a");
+        int a = scannerClavier.nextInt();
+        scannerClavier.nextLine();
+        System.out.println("Veuillez entrer un entier b");
+        int b = scannerClavier.nextInt();
+        scannerClavier.nextLine();
+        System.out.println("Veuillez entrer un entier c");
+        int c = scannerClavier.nextInt();
+        scannerClavier.nextLine();
+
+        // Chercher le maximum entre a et b (ne faîtes pas ça chez vous, il
+        // y a beaucoup mieux en mode 'Programmation Orientée Objet')
+        // et afficher le résultat
+        if (a > b) {
+            System.out.println(a);
+        } else {
+            System.out.println(b);
+        }
+
+        // Chercher le maximum entre a,b et c
+        // et afficher le résultat8
+        if (a > b && a > c) {
+            System.out.println(a);
+        } else if (b > c && b > a) {
+            System.out.println(b);
+        } else {
+            System.out.println(c);
+        }
+        // Afficher si a est pair ou impair avec un switch
+        // boolean pair = a%2 == 0;
+        // boolean impair = a%2 != 0;
+
+        switch (a % 2) {
+            case 0 -> {
+                System.out.println("a est pair!");
+            }
+            case 1 -> {
+                System.out.println("a est impair!");
+            }
+        }
+        // Afficher si a est pair ou impair avec une expression ternaire
+        System.out.println(a % 2 == 0 ? "a est pair" : "a est impair");
+        // Fermeture des ressources (il y a mieux pour ce faire, mais nous verrons cela
+        // plus tard)
+        scannerClavier.close();
+    }
+
+}
