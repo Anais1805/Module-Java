153ed028bbaef6d54fa7e15a9d45d7f23214a1dd
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-02-28 12:21:18 +0100

    exception

--- simulateur/MainOptionnel.java
+++ simulateur/MainOptionnel.java
@@ -9,6 +9,8 @@ public class MainOptionnel {
         System.out.println("Veuillez entrer un nouvelEtat");
         String nouvelEtat = scannerClavier.next();
         scannerClavier.nextLine();
+        //transformation du String nouvel etat 
+        // en variable de type Etat Bateau et en valeur de l'enum
         EtatBateau monNouvelEtat = EtatBateau.valueOf(nouvelEtat);
         System.out.println("Mon nouvel Etat est :  " + monNouvelEtat);
     }
