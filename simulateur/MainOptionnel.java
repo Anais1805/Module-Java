62bad6b496c2d791275fc1833f738c0eff37f09a
Anais1805 <111952504+Anais1805@users.noreply.github.com>
2023-02-28 12:23:19 +0100

    exceptions

--- simulateur/MainOptionnel.java
+++ simulateur/MainOptionnel.java
@@ -11,6 +11,7 @@ public class MainOptionnel {
         scannerClavier.nextLine();
         //transformation du String nouvel etat 
         // en variable de type Etat Bateau et en valeur de l'enum
+        //ssi sa valeur  est équivalente à une des valeurs de l'enum
         EtatBateau monNouvelEtat = EtatBateau.valueOf(nouvelEtat);
         System.out.println("Mon nouvel Etat est :  " + monNouvelEtat);
     }
